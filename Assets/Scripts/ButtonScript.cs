c47c484b8d8dc171b127889fa6a56ea50fd23ba1
Shikhar <shikhar.kr007@gmail.com>
2019-12-05 01:36:00 +0530

    first commit with urls and logo anim working

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+// [UnityEngine.JSONSerializeModule];
+
+// [Serializable]
+public class ButtonScript : MonoBehaviour {
+    string path;
+    string jsonString;
+    public RawImage[] buttonList;
+    private string[] webSiteUrl;
+    // Start is called before the first frame update
+    IEnumerator Start () {
+
+        var path = Resources.Load<TextAsset> ("JsonUrl");
+        jsonString = path.ToString ();
+        var urlData = dataContent.CreateFromJson (jsonString).data;
+
+        webSiteUrl = new string[] { "", "", "", "", "", "", "", "" };
+        for (int i = 0; i < 8; i++) {
+            WWW urlResponse = new WWW (urlData[i].imageUrl);
+            yield return urlResponse;
+            buttonList[i].texture = urlResponse.texture;
+            webSiteUrl[i] = urlData[i].webUrl;
+        }
+    }
+
+    [Serializable]
+    public class dataContent {
+
+        public List<Data> data;
+        [Serializable]
+        public class Data {
+            public string imageUrl;
+            public string webUrl;
+        }
+        public static dataContent CreateFromJson (string jsonString) {
+            return JsonUtility.FromJson<dataContent> (jsonString);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+
+    public void onBtnPressed (int btn) {
+        string webLink = webSiteUrl[btn];
+        Application.OpenURL (webLink);
+    }
+}
---
+++ Assets/Scripts/Logo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Logo : MonoBehaviour {
+
+    public Animation logo;
+    public AudioClip whoosh;
+    public AudioSource source;
+    public GameObject logoView;
+    public GameObject scrollView;
+
+    // Start is called before the first frame update
+    IEnumerator Start () {
+        source.clip = whoosh;
+        source.Play ();
+        logo.Play ();
+        yield return new WaitForSeconds (source.clip.length + 1);
+        logoView.SetActive (false);
+        scrollView.SetActive (true);
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+}
